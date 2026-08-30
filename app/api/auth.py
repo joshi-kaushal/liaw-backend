948b8e96c57b1ba91877a18800e16364294a3a53
Kaushal Joshi <7joshikaushal@gmail.com>
2026-08-30 17:30:20 +0530

    chore: whatsapp otp code message imrpoved

--- app/api/auth.py
+++ app/api/auth.py
@@ -43,7 +43,7 @@ async def request_otp(
     # Generate OTP
     code = await create_otp(db, user.id)
 
-    message = f"🔐 Your OTP to sign into the Live in a Week app is: {code}."
+    message = f"🔐 Your OTP to sign into the Live in a Week app is: *{code}*"
     # Send OTP via WhatsApp
     sent = await send_text_message(body.phone_number, message)
     if not sent:
